e223eb88ffc156a2b261e740b69a0e20d105bda6
Krunoslav Lujic <python.developer.edukacija@gmail.com>
2024-01-30 18:31:40 +0100

    Summary

---
+++ py_txt_01.py
@@ -0,0 +1,22 @@
+from datetime import datetime as dt
+
+
+first_name = input("Upisite Vase ime: ")
+file_name_date = dt.now().strftime('%Y-%m')
+
+
+# 1. korak - otvori konekciju prema datoteci
+file_writer = open(f'files/{file_name_date}.txt', 'w') # 'w' -> write
+# 2. korak - zapisi podatke u datoteku
+file_writer.write(first_name)
+# 3. korak - zatvori konekciju prema datoteci
+file_writer.close()
+
+# 1. korak - otvori konekciju prema datoteci
+file_reader = open(f'files/{file_name_date}.txt', 'r') # 'r' -> read
+# 2. korak - zapisi podatke u datoteku
+file_data = file_reader.read()
+# 3. korak - zatvori konekciju prema datoteci
+file_reader.close()
+
+print(f'Sadrzaj datoteke je {file_data}')
