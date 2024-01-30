94d16f5e7997735f942d1517f8ba5dd1d2f5a336
Krunoslav Lujic <python.developer.edukacija@gmail.com>
2024-01-30 18:43:40 +0100

    Summary

--- py_txt_01.py
+++ py_txt_01.py
@@ -6,14 +6,14 @@ file_name_date = dt.now().strftime('%Y-%m')
 
 
 # 1. korak - otvori konekciju prema datoteci
-file_writer = open(f'files/{file_name_date}.txt', 'w') # 'w' -> write
+file_writer = open(f'files/{file_name_date}.txt', 'w') # 'w' -> (over)write ili 'a' -> append
 # 2. korak - zapisi podatke u datoteku
 file_writer.write(first_name)
 # 3. korak - zatvori konekciju prema datoteci
 file_writer.close()
 
 # 1. korak - otvori konekciju prema datoteci
-file_reader = open(f'files/{file_name_date}.txt', 'r') # 'r' -> read
+file_reader = open(f'files\{file_name_date}.txt', 'r') # 'r' -> read
 # 2. korak - zapisi podatke u datoteku
 file_data = file_reader.read()
 # 3. korak - zatvori konekciju prema datoteci
